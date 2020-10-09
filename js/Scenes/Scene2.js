c09481a850622878a540ff82b935c225abdb003c
Paul Blackwell <blackwellpaul34@gmail.com>
2020-10-09 17:31:45 +0100

    Part 6 Physics

--- js/Scenes/Scene2.js
+++ js/Scenes/Scene2.js
@@ -5,12 +5,12 @@ class Scene2 extends Phaser.Scene {
         super('playGame')
     }
 
-    create(){
+    create() {
 
         // Add background
         //this.background = this.add.image(0,0,'background');
-        this.background = this.add.tileSprite(0,0, config.width, config.height, 'background')
-        this.background.setOrigin(0,0)
+        this.background = this.add.tileSprite(0, 0, config.width, config.height, 'background')
+        this.background.setOrigin(0, 0)
 
         // Add ships
         // this.ship1 = this.add.image(config.width/ 2 - 50, config.height/ 2, 'ship');
@@ -18,9 +18,9 @@ class Scene2 extends Phaser.Scene {
         // this.ship3 = this.add.image(config.width/ 2 + 50, config.height/ 2, 'ship3');
 
         // Add ships
-        this.ship1 = this.add.sprite(config.width/ 2 - 50, config.height/ 2, 'ship');
-        this.ship2 = this.add.sprite(config.width/ 2, config.height/ 2, 'ship2');
-        this.ship3 = this.add.sprite(config.width/ 2 + 50, config.height/ 2, 'ship3');
+        this.ship1 = this.add.sprite(config.width / 2 - 50, config.height / 2, 'ship');
+        this.ship2 = this.add.sprite(config.width / 2, config.height / 2, 'ship2');
+        this.ship3 = this.add.sprite(config.width / 2 + 50, config.height / 2, 'ship3');
 
 
         // Basic 2 frame looping animation 
@@ -32,24 +32,88 @@ class Scene2 extends Phaser.Scene {
         })
         this.anims.create({
             key: 'ship2_anim',
-            frames: this.anims.generateFrameNumbers('ship2'), // using the frames from the ship sprite sheet
+            frames: this.anims.generateFrameNumbers('ship2'),
             frameRate: 20,
             repeat: -1
         })
         this.anims.create({
             key: 'ship3_anim',
-            frames: this.anims.generateFrameNumbers('ship3'), // using the frames from the ship sprite sheet
+            frames: this.anims.generateFrameNumbers('ship3'),
             frameRate: 20,
             repeat: -1
         })
 
         this.anims.create({
             key: 'explode',
-            frames: this.anims.generateFrameNumbers('explosion'), // using the frames from the ship sprite sheet
+            frames: this.anims.generateFrameNumbers('explosion'),
             frameRate: 20,
             repeat: 0,
             hideOnComplete: true
         })
+
+
+        this.anims.create({
+            key: 'red',
+            frames: this.anims.generateFrameNumbers('power-up', {
+                start: 0,
+                end: 1
+            }),
+            frameRate: 20,
+            repeat: -1,
+        })
+
+        this.anims.create({
+            key: 'gray',
+            frames: this.anims.generateFrameNumbers('power-up', {
+                start: 2,
+                end: 3
+            }),
+            frameRate: 20,
+            repeat: -1,
+        })
+
+
+        // This will stop power ups from  colliding with each other
+        //this.physics.world.setBoundsCollision();
+
+
+        // Create power ups
+        this.powerUps = this.physics.add.group();
+        const maxObjects = 4;
+        for (let i = 0; i <= maxObjects; i++) {
+            const powerUp = this.physics.add.sprite(16, 16, 'power-up');
+            this.powerUps.add(powerUp);
+            powerUp.setRandomPosition(0, 0, game.config.width, game.config.height);
+
+            // Set power up to be red or gray (50%) chance
+            if (Math.random() > 0.5) {
+                powerUp.play('red');
+            } else {
+                powerUp.play('gray');
+            }
+
+            // Set velocity (instead of changing the speed)
+            powerUp.setVelocity(100, 100)
+
+            // Set boundaries, this will stop them going out of view
+            powerUp.setCollideWorldBounds(true);
+
+            /**
+             * This will make them bounce off each other when the, 
+             * power ups hit one another
+             */
+            this.physics.add.collider(this.powerUps, this.powerUps)
+
+            /**
+             * This will make the power ups bounce around on the screen  
+             * like if they were rubber bulls
+             */
+            powerUp.setBounce(1);
+        }
+
+
+
+
 
 
         // Play animations
@@ -75,10 +139,10 @@ class Scene2 extends Phaser.Scene {
          * 
          * - "this" is to pass the scope to the callback function
          */
-         this.input.on('gameobjectdown', this.destroyShip, this)
+        this.input.on('gameobjectdown', this.destroyShip, this)
 
 
-        this.add.text(20,20, 'Playing game', {font: '25px Arial', fill: 'yellow'})
+        this.add.text(20, 20, 'Playing game', { font: '25px Arial', fill: 'yellow' })
     }
 
 
@@ -106,9 +170,9 @@ class Scene2 extends Phaser.Scene {
         /**
          * reset position if ship moves out of the height of our game
          */
-         if(ship.y > config.height) {
-             this.restShipPos(ship)
-         }
+        if (ship.y > config.height) {
+            this.restShipPos(ship)
+        }
     }
 
 
--- js/game.js
+++ js/game.js
@@ -3,7 +3,14 @@
      width: 256,
      height: 272,
      backgroundColor: 0x000000,
-     scene: [Scene1, Scene2]
+     scene: [Scene1, Scene2],
+     pixelArt: true,
+     physics: {
+         default: 'arcade',
+         arcade: {
+             debug: false
+         }
+     }
  }
  
  
