24330b15bc96676d9d960c71a526c0c4f341bea2
Paul Blackwell <blackwellpaul34@gmail.com>
2020-10-09 10:22:34 +0100

    Part 4 Moving Images

--- js/Scenes/Scene2.js
+++ js/Scenes/Scene2.js
@@ -5,11 +5,11 @@ class Scene2 extends Phaser.Scene {
         super('playGame')
     }
 
-
     create(){
 
         // Add background
-        this.background = this.add.image(0,0,'background');
+        //this.background = this.add.image(0,0,'background');
+        this.background = this.add.tileSprite(0,0, config.width, config.height, 'background')
         this.background.setOrigin(0,0)
 
         // Add ships
@@ -19,4 +19,40 @@ class Scene2 extends Phaser.Scene {
 
         this.add.text(20,20, 'Playing game', {font: '25px Arial', fill: 'yellow'})
     }
+
+
+    update() {
+        // Move ships
+        this.moveShip(this.ship1, 1)
+        this.moveShip(this.ship2, 2)
+        this.moveShip(this.ship3, 3)
+
+        /**
+         * Move background by
+         * decreasing the position of the texture of the background 
+         * from the update function 
+         */
+        this.background.tilePositionY -= 0.5;
+    }
+
+
+
+    moveShip(ship, speed) {
+        ship.y += speed;
+
+        /**
+         * reset position if ship moves out of the height of our game
+         */
+         if(ship.y > config.height) {
+             this.restShipPos(ship)
+         }
+    }
+
+
+    restShipPos(ship) {
+        ship.y = 0;
+        const randomX = Phaser.Math.Between(0, config.width);
+        ship.x = randomX;
+    }
+
 }
